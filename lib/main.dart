8ee02c243b2f4bc1e66f321458831a7077278d2c
Sagar Sangani <sagarsangani4993@gmail.com>
2022-03-16 19:41:04 +0530

    feat: meals category page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +7,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.teal,
+          secondary: Colors.amberAccent,
+        ),
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              // bodyText1: TextStyle(
+              //   color: Color.fromRGBO(20, 51, 51, 1),
+              // ),
+              // bodyText2: TextStyle(
+              //   color: Color.fromRGBO(20, 51, 51, 1),
+              // ),
+
+              titleMedium: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RebotoCondensed',
+              ),
+            ),
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
